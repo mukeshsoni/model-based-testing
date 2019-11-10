b31c7fd5598f07826b39050431dfdd5a9ed3260b
Mukesh Soni <mukeshsoni@gmail.com>
2019-11-10 09:34:57 +0530

    refactor: allow state validation functions to be passed to machine object from outside

--- src/autocomplete_machine.js
+++ src/autocomplete_machine.js
@@ -1,3 +1,4 @@
+import cloneDeep from "clone-deep";
 import { Machine, assign } from "xstate";
 import { createModel } from "@xstate/test";
 import { fireEvent } from "@testing-library/react";
@@ -5,138 +6,7 @@ import { getSuggestions } from "./Autocomplete";
 
 const nameList = ["Rithwik", "Mukesh"];
 
-console.log({ Machine });
-function matches(ctx) {
-  return getSuggestions(ctx.text).length !== 0;
-}
-
-function doesNotMatch(ctx) {
-  return !matches(ctx);
-}
-
-function shouldHideList(ctx) {
-  return !ctx.text || ctx.text.length === 0;
-}
-
-const autocompleteMachine = Machine({
-  id: "autocomplete",
-  initial: "blur",
-  context: {
-    text: ""
-  },
-  states: {
-    blur: {
-      on: {
-        FOCUS: "focused"
-      },
-      meta: {
-        test: ({ queryByRole, container }) => {
-          expect(container.querySelector("input")).toBeDefined();
-          expect(container.querySelectorAll("[role=option]")).toHaveLength(0);
-        }
-      }
-    },
-    focused: {
-      initial: "match",
-      on: {
-        BLUR: "blur",
-        MOUSE_CLICK_OUTSIDE: "blur",
-        CHANGE_TEXT: {
-          target: "focused.match",
-          actions: [assign({ text: (ctx, e) => e.text })]
-        }
-      },
-      meta: {
-        test: ({ container }) =>
-          expect(container.querySelector("input")).toBeDefined()
-      },
-      states: {
-        match: {
-          on: {
-            "": [
-              {
-                cond: shouldHideList,
-                target: "list_hidden"
-              },
-              {
-                cond: matches,
-                target: "list.non_empty"
-              },
-              {
-                cond: doesNotMatch,
-                target: "list.empty"
-              }
-            ]
-          }
-        },
-        list_hidden: {
-          meta: {
-            test: ({ queryByRole }) =>
-              // the listbox is there but with zero elements
-              expect(
-                queryByRole("listbox").querySelectorAll("option")
-              ).toHaveLength(0)
-          }
-        },
-        list: {
-          states: {
-            empty: {
-              meta: {
-                test: ({ queryByRole }) =>
-                  // the listbox is there but with zero elements
-                  expect(
-                    queryByRole("listbox").querySelectorAll("option")
-                  ).toHaveLength(0)
-              }
-            },
-            non_empty: {
-              initial: "not_selected",
-              on: {
-                ARROW_KEY: "non_empty.selected",
-                MOUSE_CLICK_ITEM: "#autocomplete.blur"
-              },
-              meta: {
-                test: ({ container }) =>
-                  // the listbox is there but with zero elements
-                  expect(
-                    container.querySelectorAll("[role=option]").length
-                  ).toBeGreaterThanOrEqual(1)
-              },
-
-              states: {
-                not_selected: {
-                  meta: {
-                    test: ({ container }) =>
-                      expect(
-                        container.querySelectorAll(
-                          ".react-autosuggest__suggestion--highlighted"
-                        )
-                      ).toHaveLength(0)
-                  }
-                },
-                selected: {
-                  meta: {
-                    test: ({ container }) =>
-                      expect(
-                        container.querySelectorAll(
-                          ".react-autosuggest__suggestion--highlighted"
-                        )
-                      ).toHaveLength(1)
-                  },
-                  on: {
-                    ENTER_KEY: "#autocomplete.blur"
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-});
-
-const autocompleteModel = createModel(autocompleteMachine).withEvents({
+const events = {
   FOCUS: ({ container }) => fireEvent.focus(container.querySelector("input")),
   BLUR: ({ container }) => fireEvent.blur(container.querySelector("input")),
   CHANGE_TEXT: {
@@ -165,8 +35,151 @@ const autocompleteModel = createModel(autocompleteMachine).withEvents({
     fireEvent.click(randomSuggestedItem);
   },
   MOUSE_CLICK_OUTSIDE: ({ container }) =>
+    // fireEvent.click(container.querySelectorAll("*:not(input)")[0])
     fireEvent.blur(container.querySelector("input"))
-});
+};
+
+const machineObject = {
+  id: "autocomplete",
+  initial: "blur",
+  context: {
+    text: ""
+  },
+  states: {
+    blur: {
+      on: {
+        FOCUS: "focused"
+      }
+    },
+    focused: {
+      initial: "match",
+      on: {
+        BLUR: "blur",
+        MOUSE_CLICK_OUTSIDE: "blur",
+        CHANGE_TEXT: {
+          target: "focused.match",
+          actions: [assign({ text: (ctx, e) => e.text })]
+        }
+      },
+      states: {
+        match: {
+          on: {
+            "": [
+              {
+                cond: shouldHideList,
+                target: "list_hidden"
+              },
+              {
+                cond: matches,
+                target: "list.non_empty"
+              },
+              {
+                cond: doesNotMatch,
+                target: "list.empty"
+              }
+            ]
+          }
+        },
+        list_hidden: {},
+        list: {
+          states: {
+            empty: {},
+            non_empty: {
+              initial: "not_selected",
+              on: {
+                ARROW_KEY: "non_empty.selected",
+                MOUSE_CLICK_ITEM: "#autocomplete.blur"
+              },
+              states: {
+                not_selected: {},
+                selected: {
+                  on: {
+                    ENTER_KEY: "#autocomplete.blur"
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+const stateValidators = {
+  blur: ({ queryByRole, container }) => {
+    expect(container.querySelector("input")).toBeDefined();
+    expect(container.querySelectorAll("[role=option]")).toHaveLength(0);
+  },
+  focused: ({ container }) =>
+    // this does not ensure that we are testing input focus
+    expect(container.querySelector("input")).toBeDefined(),
+  "focused.list_hidden": ({ queryByRole }) =>
+    // the listbox is there but with zero elements
+    expect(queryByRole("listbox").querySelectorAll("option")).toHaveLength(0),
+  "focused.list.empty": ({ queryByRole }) =>
+    // the listbox is there but with zero elements
+    expect(queryByRole("listbox").querySelectorAll("option")).toHaveLength(0),
+  "focused.list.non_empty": ({ container }) =>
+    // the listbox is there but with zero elements
+    expect(
+      container.querySelectorAll("[role=option]").length
+    ).toBeGreaterThanOrEqual(1),
+  "focused.list.non_empty.not_selected": ({ container }) =>
+    expect(
+      container.querySelectorAll(".react-autosuggest__suggestion--highlighted")
+    ).toHaveLength(0),
+  "focused.list.non_empty.selected": ({ container }) =>
+    expect(
+      container.querySelectorAll(".react-autosuggest__suggestion--highlighted")
+    ).toHaveLength(1)
+};
+
+function matches(ctx) {
+  return getSuggestions(ctx.text).length !== 0;
+}
+
+function doesNotMatch(ctx) {
+  return !matches(ctx);
+}
+
+function shouldHideList(ctx) {
+  return !ctx.text || ctx.text.length === 0;
+}
+
+/*
+ * This function helps extend the machine object with external object
+ * which only has validation code for each state. This makes it easier
+ * to use the common machine object with state validators for different
+ * testing environments. E.g. cypress, regular jest, puppeteer etc.
+ */
+function withStateValidators(stateValidators, machine) {
+  const machineCopy = cloneDeep(machine); // quick deep copy.
+
+  Object.entries(stateValidators).forEach(([statePath, validator]) => {
+    const pathParts = statePath.split(".");
+    const newPathParts = pathParts.map(part => ["states", part]).flat();
+
+    const stateProp = newPathParts.reduce((acc, pathPart) => {
+      return acc[pathPart];
+    }, machineCopy);
+
+    stateProp.meta = stateProp.meta || {};
+
+    stateProp.meta = {
+      ...stateProp.meta,
+      test: validator
+    };
+  });
+
+  return machineCopy;
+}
+
+const autocompleteMachine = Machine(
+  withStateValidators(stateValidators, machineObject)
+);
+
+const autocompleteModel = createModel(autocompleteMachine).withEvents(events);
 
 export default autocompleteModel;
 
