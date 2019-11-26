fcefe178a0bd08bc44156c304f66eb21c3e76007
Rithwik Anand <ranand@planview.com>
2019-11-26 19:55:15 +0530

    Jest testing passing by commenting async/await in daetpikcer

--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import {
-  render,
-  fireEvent,
-  cleanup,
-  wait,
-  waitForElementToBeRemoved
-} from "@testing-library/react";
-import { Machine, assign } from "xstate";
-import { createModel } from "@xstate/test";
+import { render, cleanup } from "@testing-library/react";
+import Datepicker from "@planview/pv-uikit/lib/datepicker";
+import testPlans from "./datepicker_test_plans_jest";
 
-import Autocomplete from "./Autocomplete";
-import App from "./App";
-import testPlans from "./test_plans_jest";
+jest.mock("popper.js", () => {
+  const PopperJS = jest.requireActual("popper.js");
+
+  return class {
+    static placements = PopperJS.placements;
+
+    constructor() {
+      return {
+        destroy: () => {},
+        scheduleUpdate: () => {}
+      };
+    }
+  };
+});
 
 testPlans.forEach(plan => {
   afterEach(cleanup);
 
   plan.paths.forEach(path => {
     it(path.description, async () => {
-      const rendered = render(<Autocomplete />);
+      const rendered = render(<Datepicker />);
 
       await path.test(rendered);
     });
   });
 });
-
--- src/config/jest/jestTransform.js
+++ src/config/jest/jestTransform.js
@@ -1,3 +1,5 @@
-module.exports = require('babel-jest').createTransformer({
-    presets: ['es2015', 'react', 'stage-1'],
-})
+require("babel-polyfill"); //https://github.com/facebook/jest/issues/3126
+require("blah");
+module.exports = require("babel-jest").createTransformer({
+  presets: ["es2015", "react", "stage-1"]
+});
